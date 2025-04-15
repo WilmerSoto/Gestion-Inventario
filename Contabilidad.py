69489eaec613e44de05441edcb9709aef0a8893b
WilmerSoto <wilmer.soto@udea.edu.co>
2025-04-14 23:54:20 -0500

    refactor VentanaTransacciones to include transaction IDs in the display tables and hide the ID column

--- Contabilidad.py
+++ Contabilidad.py
@@ -130,7 +130,8 @@ class VentanaTransacciones:
         self.btn_borrar = ttk.Button(self.frame_btn, text="Borrar fila", command=self.borrar_transacciones)
         self.btn_borrar.pack(side=LEFT, padx=10, pady=10)
         
-        self.coldata = [{"text": "Fecha", "width": 100}, 
+        self.coldata = [{"text": "id", "width": 0},
+                   {"text": "Fecha", "width": 100}, 
                    {"text": "Concepto", "width": 300, "anchor": CENTER}, 
                    {"text": "Tipo", "width": 100, "anchor": CENTER}, 
                    {"text": "Monto", "width": 150}, 
@@ -168,6 +169,8 @@ class VentanaTransacciones:
             self.table2.destroy()
             
         self.table_combinada = Tableview(self.top, coldata=self.coldata, searchable=True, paginated=True)
+        
+        self.table_combinada.get_column(0).hide()
                 
         total = 0
         for transaccion in self.transacciones:
@@ -175,7 +178,8 @@ class VentanaTransacciones:
                 total += transaccion["monto"]
             elif transaccion["tipo"] == "Egreso":
                 total -= transaccion["monto"]
-            self.table_combinada.insert_row(END, values=[transaccion["fecha"], transaccion["concepto"], transaccion["tipo"],"$ {:,.0f}".format(transaccion["monto"]), "$ {:,.0f}".format(total)])
+                
+            self.table_combinada.insert_row(END, values=[transaccion["id"], transaccion["fecha"], transaccion["concepto"], transaccion["tipo"],"$ {:,.0f}".format(transaccion["monto"]), "$ {:,.0f}".format(total)])
             
         self.table_combinada.pack(side=LEFT, expand=True, fill=BOTH)
         self.table_combinada.load_table_data()
@@ -193,11 +197,14 @@ class VentanaTransacciones:
         self.table = Tableview(self.top, coldata=self.coldata_no_saldo, searchable=True, paginated=True)
         self.table2 = Tableview(self.top, coldata=self.coldata_no_saldo, searchable=True, paginated=True)
         
+        self.table.get_column(0).hide()
+        self.table2.get_column(0).hide()
+        
         for transaccion in self.transacciones:
             if transaccion["tipo"] == "Ingreso":
-                self.table.insert_row(END, values=[transaccion["fecha"], transaccion["concepto"], transaccion["tipo"],"$ {:,.0f}".format(transaccion["monto"])])
+                self.table.insert_row(END, values=[transaccion["id"], transaccion["fecha"], transaccion["concepto"], transaccion["tipo"],"$ {:,.0f}".format(transaccion["monto"])])
             elif transaccion["tipo"] == "Egreso":
-                self.table2.insert_row(END, values=[transaccion["fecha"], transaccion["concepto"], transaccion["tipo"],"$ {:,.0f}".format(transaccion["monto"])])
+                self.table2.insert_row(END, values=[transaccion["id"], transaccion["fecha"], transaccion["concepto"], transaccion["tipo"],"$ {:,.0f}".format(transaccion["monto"])])
                 
         self.table.pack(side=LEFT, expand=True, fill=BOTH)
         self.table2.pack(side=RIGHT, expand=True, fill=BOTH)
